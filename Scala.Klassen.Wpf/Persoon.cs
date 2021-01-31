a14864ae3ad14ab39c6c872e62451dee2887c049
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-01-31 12:08:51 +0100

    Add project files.

---
+++ Scala.Klassen.Wpf/Persoon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scala.Klassen.Wpf
+{
+    class Persoon
+    {
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public bool IsMan { get; set; }
+
+        public Persoon()
+        {
+        }
+        public Persoon(string naam, string voornaam, DateTime geboortedatum, bool isman)
+        {
+            Naam = naam;
+            Voornaam = voornaam;
+            Geboortedatum = geboortedatum;
+            IsMan = isman;
+        }
+
+        public override string ToString()
+        {
+            return $"{Naam} {Voornaam}";
+        }
+    }
+}
